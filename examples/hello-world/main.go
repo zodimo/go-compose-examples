269579d101f6dce0fe04491bc95112f5e5a4d166
Jaco Labuschagne <zodimo@gmail.com>
2025-12-14 21:13:19 +0200

    v0.1.4

--- examples/hello-world/main.go
+++ examples/hello-world/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/zodimo/go-compose/compose"
-	"github.com/zodimo/go-compose/compose/runtime"
+	"github.com/zodimo/go-compose/runtime"
 	"github.com/zodimo/go-compose/state"
 	"github.com/zodimo/go-compose/store"
 	"github.com/zodimo/go-compose/theme"
