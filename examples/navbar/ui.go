f24ae6f4e0d7b836eb644690272bf170928a5625
Jaco Labuschagne <zodimo@gmail.com>
2025-12-14 21:23:35 +0200

    navbar

---
+++ examples/navbar/ui.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+
+	"gioui.org/layout"
+
+	"github.com/zodimo/go-compose/compose/foundation/icon"
+	"github.com/zodimo/go-compose/compose/foundation/layout/box"
+	"github.com/zodimo/go-compose/compose/foundation/layout/column"
+	"github.com/zodimo/go-compose/compose/foundation/material3/navigationbar"
+	m3text "github.com/zodimo/go-compose/compose/foundation/material3/text"
+	"github.com/zodimo/go-compose/compose/foundation/text"
+	"github.com/zodimo/go-compose/modifiers/size"
+	"github.com/zodimo/go-compose/modifiers/weight"
+	"github.com/zodimo/go-compose/pkg/api"
+
+	mdicons "golang.org/x/exp/shiny/materialdesign/icons"
+)
+
+func UI() api.Composable {
+	return func(c api.Composer) api.Composer {
+		// State for selected item
+		// We use a pointer to int for the state value
+		selectedIdxVal := c.State("nav_selected_index", func() any { return 0 })
+		selectedIdx := selectedIdxVal.Get().(int)
+
+		items := []struct {
+			Label string
+			Icon  []byte
+		}{
+			{"Home", mdicons.ActionHome},
+			{"Search", mdicons.ActionSearch},
+			{"Settings", mdicons.ActionSettings},
+		}
+
+		return column.Column(
+			func(c api.Composer) api.Composer {
+				// Content Area
+				box.Box(
+					func(c api.Composer) api.Composer {
+						return m3text.Text(
+							fmt.Sprintf("Selected: %s", items[selectedIdx].Label),
+							m3text.TypestyleDisplayMedium,
+							text.WithTextStyleOptions(
+								text.StyleWithColor(color.NRGBA{A: 255}),
+							),
+						)(c)
+					},
+					box.WithModifier(weight.Weight(1)),
+					box.WithAlignment(layout.Center),
+				)(c)
+
+				// Navigation Bar
+				navigationbar.NavigationBar(
+					func(c api.Composer) api.Composer {
+						for i, item := range items {
+							idx := i // capture loop variable
+							navigationbar.NavigationBarItem(
+								selectedIdx == idx,
+								func() {
+									selectedIdxVal.Set(idx)
+								},
+								func(c api.Composer) api.Composer {
+									return icon.Icon(item.Icon)(c)
+								},
+								func(c api.Composer) api.Composer {
+									return m3text.Text(item.Label, m3text.TypestyleLabelMedium)(c)
+								},
+								navigationbar.EmptyModifier,
+							)(c)
+						}
+						return c
+					},
+				)(c)
+				return c
+			},
+			column.WithModifier(size.FillMax()),
+		)(c)
+	}
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ toolchain go1.24.11
 require (
 	gioui.org v0.9.0
 	github.com/zodimo/go-compose v0.1.5
+	golang.org/x/exp/shiny v0.0.0-20250711185948-6ae5c78190dc
 )
 
 require (
@@ -14,7 +15,6 @@ require (
 	git.sr.ht/~schnwalter/gio-mw v0.0.0-20250713180710-9d8d98474447 // indirect
 	github.com/go-text/typesetting v0.3.0 // indirect
 	github.com/zodimo/go-maybe v0.1.3 // indirect
-	golang.org/x/exp/shiny v0.0.0-20250711185948-6ae5c78190dc // indirect
 	golang.org/x/image v0.29.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
